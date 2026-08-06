10375079ba26c60fd547118d096c531084c1f4fd
Javeria Razzaq <javeria.techit123@gmail.com>
2026-08-07 04:24:37 +0500

    Created requiremetns.txt and procfile and deleted some unnecessary files.

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
